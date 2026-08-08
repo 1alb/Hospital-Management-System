db9ce5b1fffb5e8a3a202fc2c218a94dcd0176fd
Monisha Gaur <163425822+1alb@users.noreply.github.com>
2026-08-08 20:00:02 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,16 @@
+```php
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "edoc";
+
+$database = new mysqli($servername, $username, $password, $dbname);
+
+if ($database->connect_error) {
+    die("Échec de la connexion : " . $database->connect_error);
+}
+?>
+```
+
+
